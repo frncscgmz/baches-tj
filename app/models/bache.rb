3183467ecb2c19757aa396da3b3f85fca74eef1b
Francisco Gomez <mail@frncscgmz.me>
2014-02-01 16:24:26 -0800

    Validaciones baches

--- app/models/bache.rb
+++ app/models/bache.rb
@@ -1,2 +1,7 @@
 class Bache < ActiveRecord::Base
+   belongs_to :user
+   validates :desc, presence: true, length: { maximum: 140 }
+   validates :user_id, presence: true
+   validates :latitude, presence: true
+   validates :longitude, presence: true
 end
