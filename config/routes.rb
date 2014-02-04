1675271c99b49cb110daa8c2925414b452c15275
Francisco Gomez <mail@frncscgmz.me>
2014-02-04 12:59:20 -0800

    Inicio de forma signup, rutas y controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 BachesTj::Application.routes.draw do
+   resources :users, only: [:create, :destroy]
+
    root 'static_pages#home'
+   match '/signup',  to: 'users#new',        via: 'get'
+
    match '/help',  to: 'static_pages#help',  via: 'get'
    match '/about', to: 'static_pages#about', via: 'get'
 
