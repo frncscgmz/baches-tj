9b00f7a962c4a70c09f519c8a807382ca6447572
Francisco Gomez <mail@frncscgmz.me>
2014-02-04 16:08:32 -0800

    Signup completado satisfactoriamente

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
      @user = User.new(user_params)
      if @user.save
-        #
+        flash[:success] = "Bienvenido!"
+        redirect_to root_url
      else
         render 'new'
      end
