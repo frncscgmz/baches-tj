08e3a744615cb02b262174bab24ad516ead4087a
Francisco Gomez <mail@frncscgmz.me>
2014-02-21 11:44:10 -0800

    Foto en modelo de bache

--- app/models/bache.rb
+++ app/models/bache.rb
@@ -4,4 +4,10 @@ class Bache < ActiveRecord::Base
    validates :desc, presence: true, length: { maximum: 140 }
    validates :latitude, presence: true
    validates :longitude, presence: true
+
+   # Manejo de imagenes
+   has_attached_file :foto, :styles => { 
+      :medium => "500x500>", :thumb => "100x100>" }
+      , :default_url => "/images/:style/missing.png"
+   validates_attachment_content_type :foto, :content_type => /\Aimage\/.*\Z/
 end
