736cbe99226139120bb0bdf8905eee95e68697ad
Francisco Gomez <mail@frncscgmz.me>
2014-02-25 12:45:36 -0800

    Metodo en controlador twitter

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,20 +1,51 @@
 class AuthenticationsController < ApplicationController
-  def index
-    @authentications = Authentication.all
-  end
+   def index
+      @authentications = Authentication.all
+   end
 
-  def create
-    @authentication = Authentication.new(params[:authentication])
-    if @authentication.save
-      redirect_to authentications_url, :notice => "Successfully created authentication."
-    else
-      render :action => 'new'
-    end
-  end
+   def twitter
+      omni = request.env["omniauth.auth"]
+      authentication = Authentication.find_by_provider_and_uid(omni['provider'], omni['uid'])
 
-  def destroy
-    @authentication = Authentication.find(params[:id])
-    @authentication.destroy
-    redirect_to authentications_url, :notice => "Successfully destroyed authentication."
-  end
+      if authentication
+         flash[:notice] = "Logged in Successfully"
+         sign_in_and_redirect root_url
+      elsif current_user
+         token = omni['credentials'].token
+         token_secret = omni['credentials'].secret
+
+         current_user.authentications.create!(:provider => omni['provider'], :uid => omni['uid'], :token => token, :token_secret => token_secret)
+         flash[:notice] = "Authentication successful."
+         sign_in_and_redirect root_url
+      else
+         user = User.new
+         user.provider = omni.provider
+         user.uid = omni.uid
+
+         user.apply_omniauth(omni)
+
+         if user.save
+            flash[:notice] = "Logged in."
+            sign_in_and_redirect root_url
+         else
+            session[:omniauth] = omni.except('extra')
+            redirect_to new_user_registration_path
+         end
+      end 
+   end
+
+   def create
+      @authentication = Authentication.new(params[:authentication])
+      if @authentication.save
+         redirect_to authentications_url, :notice => "Successfully created authentication."
+      else
+         render :action => 'new'
+      end
+   end
+
+   def destroy
+      @authentication = Authentication.find(params[:id])
+      @authentication.destroy
+      redirect_to authentications_url, :notice => "Successfully destroyed authentication."
+   end
 end
