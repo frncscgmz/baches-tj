8b7e51b729a33b4f09ef51e7f93157dcd06cf093
Francisco Gomez <mail@frncscgmz.me>
2014-02-25 12:19:25 -0800

    Nueva tabla authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 BachesTj::Application.routes.draw do
-  resources :authentications
-  devise_for :users
+   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+      controllers: {omniauth_callbacks: "authentications",
+         registrations: "registrations"}
    resources :baches,   only: [:index, :create]
-   resources :users,    only: [:create, :destroy]
-   resources :sessions, only: [:new, :create, :destroy]
+   resources :authentications
+   #resources :users,    only: [:create, :destroy]
+   #resources :sessions, only: [:new, :create, :destroy]
 
    root to: 'baches#index', via: 'get'
 
-   match '/signup',  to: 'users#new',        via: 'get'
-   match '/signin',  to: 'sessions#new',     via: 'get'
-   match '/signout', to: 'sessions#destroy', via: 'delete'
+   #match '/signup',  to: 'users#new',        via: 'get'
+   #match '/signin',  to: 'sessions#new',     via: 'get'
+   #match '/signout', to: 'sessions#destroy', via: 'delete'
 
    match '/help',    to: 'static_pages#help',  via: 'get'
    match '/about',   to: 'static_pages#about', via: 'get'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225004033) do
+ActiveRecord::Schema.define(version: 20140225194204) do
+
+  create_table "authentications", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "baches", force: true do |t|
     t.string   "desc"
