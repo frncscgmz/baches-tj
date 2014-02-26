2e1c7b264960bcf6c314fa99898d89ab89d15701
Francisco Gomez <mail@frncscgmz.me>
2014-02-25 18:18:57 -0800

    Agregar columnas provider y uid a users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225194204) do
+ActiveRecord::Schema.define(version: 20140226021637) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20140225194204) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
