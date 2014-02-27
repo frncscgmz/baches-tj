77f5b4c40a6ce3c251676337d0483829c41e4be4
Francisco Gomez <mail@frncscgmz.me>
2014-02-26 18:54:16 -0800

    Configurar parametros default de devise y extender con name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ActiveRecord::Base
    has_many :authentications, :dependent => :destroy
    has_many :bache, dependent: :destroy
 
-   #before_save { self.email = email.downcase }
+   before_save { self.email = email.downcase }
    #before_create :create_remember_token
 
-   #validates :name, presence: true, length: { maximum: 50 }
+   validates :name, presence: true, length: { maximum: 50 }
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   #validates :email, presence: true,
-      #format: { with: VALID_EMAIL_REGEX },
-      #uniqueness: { case_sensitive: false }
+   validates :email, presence: true,
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
    #has_secure_password
    #validates :password, length: { minimum: 6 }
 
