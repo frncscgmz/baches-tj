87e1e25a834e0c97ae8bc4a0085bf70d0583537d
Francisco Gomez <mail@frncscgmz.me>
2014-02-27 22:06:50 -0800

    Configs para Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BachesTj::Application.config.secret_key_base = '89dc1d7d1d781fbd9bef223cc550aad039dc88bd81bf6d37d625dd9c6efa6853c1c994618572de3c9799c2f6c0cf9e96a0b5a92e1bb3e31ebf2b4d5a015ef902'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BachesTj::Application.config.session_store :cookie_store, key: '_baches-tj_session'
